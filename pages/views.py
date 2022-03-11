6abdc57328c81854aed465a6281b3173edc93766
Joetib <otiboatengjoe@gmail.com>
2022-03-11 02:24:39 +0000

    Added team members section in about page

--- pages/views.py
+++ pages/views.py
@@ -13,6 +13,11 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['team_members'] = models.TeamMember.objects.all()
+        return context
+
 
 class InvestmentListView(ListView):
     model = models.InvestmentOpportunity
