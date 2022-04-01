4aa15abe05969c9de2fec49c93e0ff5d85bde2ce
Oti Boateng Joseph <otiboatengjoe@gmail.com>
2022-04-01 11:33:16 +0000

    Added search to businessnewslistview

--- investment_news/views.py
+++ investment_news/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from django.db.models import Q
 from investment_news.models import BusinessNews
 
 # Create your views here.
@@ -12,6 +12,13 @@ class BusinessNewsList(ListView):
     context_object_name = "business_news_list"
     template_name = "business_news/business_news_list.html"
 
+    def get_queryset(self):
+        queryset = self.queryset
+        self.search_term = self.request.GET.get("search", "")
+        if self.search_term:
+            search_term_list = self.search_term.strip().split()
+            queryset = queryset.filter(Q(title__icontains=search_term_list)|Q(description__icontains=search_term_list) | Q(content__icontains=self.search_term))
+        return queryset
 
 class BusinessNewsDetail(DetailView):
     model = BusinessNews
